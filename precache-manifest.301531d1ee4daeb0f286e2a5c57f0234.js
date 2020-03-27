5de4622df66f3265e4ebd42ba71929bb6e942cab
Rohit Boyina <25886790+rboyina@users.noreply.github.com>
2020-03-27 16:52:18 -0400

    Updates

---
+++ precache-manifest.301531d1ee4daeb0f286e2a5c57f0234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fc6516769036aa6ca52740481d6610",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d30b021ad69313ca34d",
+    "url": "/monsters-rolodex/static/css/main.9a6c89f7.chunk.css"
+  },
+  {
+    "revision": "21d8d05ee25466752c45",
+    "url": "/monsters-rolodex/static/js/2.20e4bcd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.20e4bcd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d30b021ad69313ca34d",
+    "url": "/monsters-rolodex/static/js/main.6b1d575a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
